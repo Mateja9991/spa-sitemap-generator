478e400f997bf2324fb297af91e761c2d418dde2
Mateja <zlatanovic007@gmail.com>
2022-09-01 11:55:46 +0200

    name change

--- src/index.js
+++ src/index.js
@@ -1,18 +1,16 @@
-const { Command, program } = require("commander");
-const Crawler = require("./crawler");
+const { Command, program } = require('commander');
+const Crawler = require('./crawler');
 
-program.option("-f, --file-name <value>", "name of generated xml file");
-program.option("-p, --path <value>", "path to the generated xml file");
+program.option('-f, --file-name <value>', 'name of generated xml file');
+program.option('-p, --path <value>', 'path to the generated xml file');
 program.parse();
 const options = program.opts();
 const { fileName, path } = options;
 try {
-    if (!program.args.length)
-        throw new Error("You need to pass url as argument");
-    const url = program.args[0];
-    console.log(options);
-    const crawler = new Crawler(url, path, fileName);
-    crawler.startCrawling();
+  if (!program.args.length) throw new Error('You need to pass url as argument');
+  const url = program.args[0];
+  const crawler = new Crawler(url, path, fileName);
+  crawler.startCrawling();
 } catch (err) {
-    console.log(err);
+  console.log(err);
 }
